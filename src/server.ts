8d8cdf53b46cd99a777f8f23931ea2e5c94fce0e
Junior Volonte <juniorvolonte.com.br@gmail.com>
2023-10-17 08:43:46 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { env } from './env'
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => console.log('Server is running'))
